3bc1d50f6be445d0c4bd0eedf9cab82e05c59105
matt lee <crazoter@gmail.com>
2020-06-27 22:56:20 +0800

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# timetable-pinger-py-
+# timetable-pinger-py
+
 Using AWS Lambda + Cloudwatch to read your timetable from Google Sheets and keep you updated with Telegram bot API
+
+## How?
+
+* Basically you use Google Sheets as your  timetable. With every new week, duplicate the tab and update the dates on the 2nd row (just update the first cell then drag across the rest). 
+* Fill in your timetable (you can use the fill helper at the bottom right, but it's not very well documented :p). Each cell is half an hour.
+* The AWS lambda (once setup) will read the last sheet (which is presumably the sheet with the latest date) and compare the current cell with the previous cell. If there is a difference, it will send you a message using Telegram.
+
+## [AWS Lambda Config Parameters](https://docs.aws.amazon.com/lambda/latest/dg/configuration-envvars.html)
+Necessary:
+* **SPREADSHEET_ID**: Your spreadsheet ID.
+* **SERVICE_ACCOUNT_SECRET**: The secret JSON key for your service account. Copy the entire json in. Make sure your env. variables are encrypted.
+* **TELEGRAM_BOT_TOKEN**: Token which identifies your bot, used for Telegram bot API.
+* **TELEGRAM_CHAT_ID**: The bot will use this chat id to send you notifications.
+* **UTC_TIME_DIFFERENCE**: Time difference from UTC (e.g. Singapore is UTC+8, so it would be "8"). Unfortunately current implementation probably doesn't account for daylight saving time
+
+Optional:
+* **TRIGGER_INTERVAL**: Integer, set this as the interval (in minutes) you set your cloudwatch to trigger the lambda code. Default 5.
+* **INACTIVE_START_HOUR** Integer, start hour (inclusive) where the lambda stops sending pings (e.g. for when you are sleeping). Range 0-23, default 0.
+* **INACTIVE_END_HOUR** Integer, end hour (exclusive) where the lambda stops sending pings (e.g. for when you are sleeping). Range 0-23, default 0.
+* **DEBUG_MODE**: If set to a value, then messages will be more verbose. Telegram bot will also receive debug messages.
+
+## Steps
+
+## Integrating Google Sheets
+1. I use this [template](https://docs.google.com/spreadsheets/d/15L7JJgl1YgaGM9b8nN6eNuTSHaSWa5_IQUya8mL9dXw/edit?usp=sharing) for my own timetabling purposes. You can make a copy. Note that the date format matters (17 Jun, 18 Feb etc)
+2. There are numerous tutorials online on how to access google sheets. [The one I used](https://medium.com/@denisluiz/python-with-google-sheets-service-account-step-by-step-8f74c26ed28e) didn't involve gspread.
+3. Although you don't need to setup the code, you'll need to undergo the steps in the tutorial to obtain:
+    * SPREADSHEET_ID
+    * SERVICE_ACCOUNT_SECRET
+    * Make sure the lambda can access your spreadsheet
+
+## Integrating Telegram
+1. Create a telegram bot (See [this](https://core.telegram.org/bots)).
+2. Get it's **token**.
+3. Send it a start message via Telegram using your account. The bot will send you messages via this chat.
+    * Using your browser, call the [getUpdates method](https://core.telegram.org/bots/api#getupdates) using [Telegram Bot API](https://core.telegram.org/bots/api#making-requests). You can identify the **chat_id** this way.
+
+## Integrating AWS Lambda & AWS Cloudwatch
+1. Make sure you have an AWS account. Using this application uses about ~10k lambda requests per month if you set the scheduler to ping every 5 minutes, so it's definitely still within the free tier. 
+2. Take the zip file and upload it onto AWS lambda. You can follow [this](https://docs.aws.amazon.com/lambda/latest/dg/getting-started-create-function.html) to create a function, but you'll need to click Actions > upload zip file to upload it (the UI may change in the future though).
+    * If you want to create your own zipfile (development package) refer to [this](https://docs.aws.amazon.com/lambda/latest/dg/python-package.html#python-package-venv).
+3. Use AWS Cloudwatch to trigger scheduled lambdas. See [this](https://docs.aws.amazon.com/AmazonCloudWatch/latest/events/RunLambdaSchedule.html) tutorial.
+4. Set all the necessary environmental variables for the lambda (See "AWS Lambda Config Parameters above).
+
+## Others directory
+* Contains development package for googleapiclient & google-oauth2-tools for AWS Lambda (necessary for running python code that uses these libraries on AWS Lambda)
---
+++ lambda_function.py
@@ -0,0 +1,182 @@
+import datetime
+import json
+import os
+import urllib.parse
+import urllib.request
+
+# You'll need to setup a development pack for these
+# https://aws.amazon.com/premiumsupport/knowledge-center/build-python-lambda-deployment-package/
+# make sure to install python3-pip and call using pip3
+# https://stackoverflow.com/questions/4495120/combine-user-with-prefix-error-with-setup-py-install
+# Also see: "with a virtual environment"
+# https://docs.aws.amazon.com/lambda/latest/dg/python-package.html#python-package-venv
+from googleapiclient import discovery
+from google.oauth2 import service_account
+
+# AWS LAMBDA ENVIRONMENT VARIABLES
+# See https://docs.aws.amazon.com/lambda/latest/dg/configuration-envvars.html
+
+SPREADSHEET_ID = str(os.environ['SERVICE_ACCOUNT_SECRET'])
+SERVICE_ACCOUNT_SECRET = str(os.environ['SERVICE_ACCOUNT_SECRET'])
+TELEGRAM_BOT_TOKEN = str(os.environ['TELEGRAM_BOT_TOKEN'])
+TELEGRAM_CHAT_ID = str(os.environ['TELEGRAM_CHAT_ID'])
+UTC_TIME_DIFFERENCE = int(os.environ['UTC_TIME_DIFFERENCE'])
+
+TRIGGER_INTERVAL = os.environ['TRIGGER_INTERVAL']
+INACTIVE_START_HOUR = os.environ['INACTIVE_START_HOUR']
+INACTIVE_END_HOUR = os.environ['INACTIVE_END_HOUR']
+DEBUG_MODE = os.environ['DEBUG_MODE']
+
+if not TRIGGER_INTERVAL:
+    TRIGGER_INTERVAL = 5
+else:
+    TRIGGER_INTERVAL = int(TRIGGER_INTERVAL)
+if not INACTIVE_START_HOUR:
+    INACTIVE_START_HOUR = 0
+else:
+    INACTIVE_START_HOUR = int(INACTIVE_START_HOUR)
+if not INACTIVE_END_HOUR:
+    INACTIVE_END_HOUR = 8
+else:
+    INACTIVE_END_HOUR = int(INACTIVE_END_HOUR)
+
+# Other constants specific to the spreadsheet
+DAY_JUMP_INDEX = 3
+day_col_index = [i for i in range(0, 7 * DAY_JUMP_INDEX, DAY_JUMP_INDEX)]
+DATA_START_INDEX = 3
+hour_row_index = [i for i in range(DATA_START_INDEX, 24 + DATA_START_INDEX)]
+
+# Other constants
+SCOPES = ["https://www.googleapis.com/auth/drive", 
+    "https://www.googleapis.com/auth/drive.file", 
+    "https://www.googleapis.com/auth/spreadsheets"
+]
+
+def ret_msg(text):
+    if DEBUG_MODE:
+        return text
+    else:
+        return "operational"
+
+def get_formatted_date(datetime_object):
+    # Return "17 Jun" for example
+    # https://stackabuse.com/converting-strings-to-datetime-in-python/
+    return datetime_object.strftime('%d %b')
+
+def get_day_str(sheet_data, day_index):
+    # Get day from ss headers
+    col_index = day_col_index[day_index]
+    return sheet_data["sheets"][0]["data"][0]["rowData"][0]["values"][col_index]["formattedValue"]
+
+def get_item_str(sheet_data, day_index, hour, is_second_half):
+    # Get item from ss
+    col_index = day_col_index[day_index] + (1 if is_second_half else 0)
+    row_index = hour_row_index[hour]
+    return sheet_data["sheets"][0]["data"][0]["rowData"][row_index]['values'][col_index]['formattedValue']
+
+def get_item_str_from_datetime(sheet_data, datetime_object):
+    day_index = get_day_index(sheet_data, datetime_object)
+    hour = datetime_object.hour
+    is_second_half = datetime_object.minute >= 30
+    return get_item_str(sheet_data, day_index, hour, is_second_half)
+
+def get_day_index(sheet_data, datetime_object):
+    # Iteratively check if the given day string is equals to anyy
+    day_str = get_formatted_date(datetime_object)
+    for i in range(len(day_col_index)):
+        if day_str == get_day_str(sheet_data, i):
+            return i
+    raise ValueError("Could not find day_index for " + day_str)
+
+def is_inactive_time(datetime_object):
+    # Check it's not night
+    return not (datetime_object.hour > INACTIVE_END_HOUR or datetime_object.hour < INACTIVE_START_HOUR)
+
+def get_previous_item_str(sheet_data, datetime_object):
+    # Assumes same day, doesn't work if it needs to check a previous sheet
+    day_index = get_day_index(sheet_data, datetime_object)
+    hour = datetime_object.hour
+    is_second_half = datetime_object.minute >= 30
+    if is_second_half:
+        is_second_half = 0
+    else:
+        hour -= 1
+        is_second_half = 1
+    if (hour < 0):
+        day_index -= 1
+        hour = 23
+    # if day < 0: unsupported
+    return get_item_str(sheet_data, day_index, hour, is_second_half)
+    
+def send_telegram_message(text):
+    text = str(text)
+    # https://www.twilio.com/blog/2016/12/http-requests-in-python-3.html
+    # https://stackoverflow.com/questions/40557606/how-to-url-encode-in-python-3
+    url = 'https://api.telegram.org/bot' + TELEGRAM_BOT_TOKEN \
+        + '/sendMessage?chat_id=' + TELEGRAM_CHAT_ID + '&text='
+    payload = urllib.parse.quote(text)
+    url += payload
+    urllib.request.urlopen(url)
+
+def is_time_to_ping(datetime_object):
+    # Only ping after every hour or half hour
+    return (datetime_object.minute - TRIGGER_INTERVAL < 0) \
+        or (datetime_object.minute > 30 and datetime_object.minute - TRIGGER_INTERVAL <= 30)
+    
+def lambda_handler(event, context):
+    if not SPREADSHEET_ID or not SERVICE_ACCOUNT_SECRET or not TELEGRAM_BOT_TOKEN \
+        or not TELEGRAM_CHAT_ID or not UTC_TIME_DIFFERENCE:
+        return { 'statusCode': 500, 'body': json.dumps('A required environment variable is not set') }
+    
+    datetime_object = datetime.datetime.strptime(event["time"], '%Y-%m-%dT%H:%M:%SZ')
+    datetime_object += datetime.timedelta(hours=UTC_TIME_DIFFERENCE)
+
+    if DEBUG_MODE:
+        send_telegram_message("Event time: " + str(event["time"]))
+        send_telegram_message("Processed time: " + str(datetime_object.strftime("%Y-%m-%d %H:%M")))
+
+    if not is_time_to_ping(datetime_object):
+        if DEBUG_MODE:
+            send_telegram_message("not is_time_to_ping")
+        return { 'statusCode': 200, 'body': json.dumps(ret_msg('It is not yet time to ping user')) }
+
+    if is_inactive_time(datetime_object):
+        if DEBUG_MODE:
+            send_telegram_message("is_inactive_time")
+        return { 'statusCode': 200, 'body': json.dumps(ret_msg('User on silent mode at this time')) }
+
+    try:
+        # Setup access
+        key_json = json.loads(SERVICE_ACCOUNT_SECRET)
+        creds = service_account.Credentials.from_service_account_info(key_json, scopes=SCOPES)
+        service = discovery.build('sheets', 'v4', credentials=creds)
+        # See for API: http://googleapis.github.io/google-api-python-client/docs/dyn/sheets_v4.spreadsheets.html     
+        # Get last sheet
+        # https://stackoverflow.com/questions/38245714/get-list-of-sheets-and-latest-sheet-in-google-spreadsheet-api-v4-in-python
+        sheet_metadata = service.spreadsheets().get(spreadsheetId=SPREADSHEET_ID).execute()
+        sheets = sheet_metadata.get('sheets', '')
+        sheet_index = len(sheets) - 1
+        sheet_title = sheets[sheet_index].get("properties", {}).get("title", "Sheet1")
+        # Generate range name
+        range_name = "\'" + sheet_title + "\'!B2:U28"
+        # Get sheet data
+        sheet_data = service.spreadsheets().get(spreadsheetId=SPREADSHEET_ID, ranges=range_name, includeGridData=True).execute()
+
+        now_item = get_item_str_from_datetime(sheet_data, datetime_object)
+        prev_item = get_previous_item_str(sheet_data, datetime_object)
+        if (now_item != prev_item):
+            if DEBUG_MODE:
+                send_telegram_message("Old Item: " + prev_item)
+            send_telegram_message("New Item: " + now_item)
+            return { 'statusCode': 200, 'body': json.dumps(ret_msg("ping sent")) }
+        else:
+            if DEBUG_MODE:
+                send_telegram_message("Items the same: " + now_item)
+            return { 'statusCode': 200, 'body': json.dumps(ret_msg("ping not sent as items were the same")) }
+    except ValueError as error:
+        if DEBUG_MODE:
+            send_telegram_message(error)
+        return { 'statusCode': 500, 'body': json.dumps(ret_msg(error)) }
+
+    # Process should not reach this point
+    return { 'statusCode': 204 }
